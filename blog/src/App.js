d9773b5eac9aa8eaed37a6d6f47f4799c055b594
mathhyeonho <plato9946@gmail.com>
2022-05-08 00:45:38 +0900

    hihi

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
+  
+  let posts = '강남 고기 맛집'; 
+  function 함수(){
+    return 100
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div style = { { color : 'blue', fontSize : '30px'} }>개발 Blog</div>
+      </div>
+      <h4> {posts} </h4>
+      <h4> { 함수() }</h4>
+      <img src = {logo}/>
     </div>
   );
 }
+// react를 쓰는 이유 : 데이터 바인딩이 쉽다 위에서의 let posts 한뒤 {posts}이런 느낌
+// 중괄호가 포인트
+
 
 export default App;
